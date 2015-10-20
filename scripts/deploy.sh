af80ba3d7f09558d6e5c7685529552f27116a4f6
Abner Chen <houjunchen@gmail.com>
2015-10-20 12:24:13 +0800

    Update deploy scripts for staging server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,7 @@
-s3cmd sync --delete-removed --acl-public --exclude-from 'scripts/s3ignore' ./ s3://autoinsight.io/
+if [ "$1" = "pro" ]; then
+    s3cmd sync --delete-removed --acl-public --exclude-from 'scripts/s3ignore' ./ s3://autoinsight.io/
+elif [ "$1" = "stg" ]; then
+    s3cmd sync --delete-removed --acl-public --exclude-from 'scripts/s3ignore' ./ s3://devel-web.autoinsight.io/
+else
+    echo "Use 'pro' or 'stg' to specify production or staging server"
+fi
