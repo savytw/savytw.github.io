28074b2124ca0eeefec5a16ac01e0ca189b077dd
Abner Chen <houjunchen@gmail.com>
2015-10-22 00:24:34 +0800

    Add --cf-invalidate in s3cmd sync to invalidate objects on CloudFront

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,16 @@
+base_cmd="s3cmd --cf-invalidate --cf-invalidate-default-index --acl-public --recursive --delete-removed --exclude-from 'scripts/s3ignore' sync ./"
+
 if [ "$1" = "pro" ]; then
-    s3cmd sync --delete-removed --acl-public --exclude-from 'scripts/s3ignore' ./ s3://autoinsight.io/
+    dest="s3://autoinsight.io/"
 elif [ "$1" = "stg" ]; then
-    s3cmd sync --delete-removed --acl-public --exclude-from 'scripts/s3ignore' ./ s3://devel-web.autoinsight.io/
+    dest="s3://devel-web.autoinsight.io/"
 else
     echo "Use 'pro' or 'stg' to specify production or staging server"
 fi
+
+cmd="$base_cmd $dest"
+eval $cmd
+
+echo "================================================================================"
+echo $cmd
+echo "================================================================================"
